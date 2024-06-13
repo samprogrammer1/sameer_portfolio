1f651ae19ff3a41a610253c61075adc041f4857e
sameer <samprogrammer1>
2024-06-13 17:17:21 +0530

    add meta tegs

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,11 @@ import "./globals.css";
 const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sameer Ali - Software Engineer | Utthan IT Solution",
-  description: "Sameer Ali is a skilled software engineer at Utthan IT Solution, specializing in web development, mobile applications, and more.",
+  title: "Sameer Ali | Software Engineer ",
+  description: "Sameer Ali is a skilled software engineer at Utthan IT Solution, specializing in web development, mobile applications, and more. Explore my portfolio to see my latest projects and learn more about my skills.",
   googleSiteVerification: "17Xz9xFTbYcuQdDNt8VDQzQRrvt8movxDAXIbbp6P3Q",
 };
+
 
 export default function RootLayout({
   children,
@@ -26,8 +27,31 @@ export default function RootLayout({
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <meta name="keywords" content="sameer, sameer ali , samprogrammer, software developer" />
+        <meta name="keywords" content="sameer ali, samprogrammer, software developer, web development, mobile applications, Utthan IT Solution" />
         <meta name="google-site-verification" content={metadata.googleSiteVerification} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://yourportfolio.com" />
+
+        {/* Structured Data for SEO */}
+        <Script type="application/ld+json" id="structured-data">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Sameer Ali",
+              "jobTitle": "Software Engineer",
+              "worksFor": {
+                "@type": "Organization",
+                "name": "Utthan IT Solution"
+              },
+              "url": "https://yourportfolio.com",
+              "sameAs": [
+                "https://www.linkedin.com/in/sameer-ali/",
+                "https://github.com/samprogrammer"
+              ]
+            }
+          `}
+        </Script>
       </Head>
       <body
         className={`${nunito.className} bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
